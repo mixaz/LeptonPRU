e9d5c3d01669397ff9029a444fccfda354c27e38
Mikhail Zemlyanukha <gmixaz@gmail.com>
2019-03-04 21:50:34 +0300

    added support for PocketBeagle

---
+++ kernel/enable-leptonpru_pb-pins.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# PocketBeagle pinout
+
+config-pin P2_30 pruout
+config-pin P2_28 pruin
+config-pin P2_32 pruout
--- kernel/pincfg.sh
+++
@@ -1,3 +0,0 @@
-config-pin P8_12 pruout
-config-pin P9_27 pruin
-config-pin P9_30 pruout
